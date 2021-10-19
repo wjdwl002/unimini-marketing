6f4abaf96214e72f75ff242fc2c0e825635d175e
wjdwl002 <wjdwl001@khu.ac.kr>
2021-10-19 12:47:21 +0900

    google spreadsheet API 추가

--- js/script.js
+++ js/script.js
@@ -58,16 +58,90 @@ function hasScrolled(){
   });
 
 
-  /*shareLink*/
+/*shareLink*/
+var snackbar = document.getElementById("alertLinkCopied")
   $(document).ready(function(){
     var clipboard = new Clipboard('.shareLinkBtn');
+    snackbar.innerHTML="클립보드에 복사되었습니다."
     clipboard.on('success',function(e){
       console.log("copy success");
-      document.getElementById("alertLinkCopied").classList.replace("alertInactive","alertActive");
+      snackbar.classList.replace("alertInactive","alertActive");
       setTimeout(function(){
-        document.getElementById("alertLinkCopied").classList.replace("alertActive","alertInactive");
+        snackbar.classList.replace("alertActive","alertInactive");
       },3500)
     })
   })
   console.log("js");
-  
+
+
+/*spread sheet API*/
+snackbar.innerHTML="베타 신청이 완료되었습니다."
+var inputs = $('input[type="text"]');
+var googleSubmitBtn = $('#bigButtonJoinBeta');
+
+var inputName = $('#name');
+var inputNumber = $('#number');
+var inputEmail = $('#email');
+
+function isLoading(status){
+  if(status){
+    $('html, body').addClass('wait');
+    googleSubmitBtn.attr('disabled', true).html('신청중...');
+  } else {
+    $('html, body').removeClass('wait');
+    googleSubmitBtn.attr('disabled', false).html('베타 신청하기');
+  }
+}
+
+function checkInput(){
+  var isEmpty = false;
+  $.each(inputs, function (index, element) {
+    if (element.value === '') {
+      snackbar.innerHTML="빈 칸이 있어요."
+      snackbar.classList.replace("alertInactive","alertActive");
+      setTimeout(function(){
+        snackbar.classList.replace("alertActive","alertInactive");
+      },3000)
+      isEmpty = true;
+      return false;
+    }
+  });
+  return isEmpty;
+}
+
+$('#bigButtonJoinBeta').click(function () {
+
+  //빈값 체크
+  if (checkInput()) { return; }
+
+  // 입력중..
+  isLoading(true);
+
+  $.ajax({
+    type: "GET",
+    url: "https://script.google.com/macros/s/AKfycbzt-fywZAoS_aIxXRKJgdrwH0cSkh1i5TIPr-76YcOKVXUZF8dkfACbvsW_pkNbcNzs/exec",
+    data: {
+      "이름": inputName.val(),
+      "학번": inputNumber.val(),
+      "이메일": inputEmail.val()
+    },
+    success: function (response) {
+      isLoading(false);
+
+      snackbar.classList.replace('alertInactive','alertActive');
+      setTimeout(function () {
+        snackbar.classList.replace('alertActive','alertInactive');
+      }, 3500);
+
+      //값 비워주기
+      inputName.val('');
+      inputNumber.val('');
+      inputEmail.val('');
+    },
+    error: function (request, status, error) {
+      isLoading(false);
+      console.log("code:" + request.status + "\n" + "error:" + error);
+      console.log(request.responseText);
+    }
+  });
+});
